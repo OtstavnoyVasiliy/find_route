078b4c71039d466ef8e729ac10625efc2831e567
Vasiliy <vasya2304@bk.ru>
2022-12-11 07:35:19 +0300

    Отображение на основе класса DetailView

--- cities/views.py
+++ cities/views.py
@@ -8,11 +8,11 @@ __all__ = (
 )
 
 def home(request, pk=None):
-    if pk:
-        #city = City.objects.filter(id=pk).first()
-        city = get_object_or_404(City, id=pk)
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    # if pk:
+    #     #city = City.objects.filter(id=pk).first()
+    #     city = get_object_or_404(City, id=pk)
+    #     context = {'object': city}
+    #     return render(request, 'cities/detail.html', context)
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)
