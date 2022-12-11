2b483a135448f5bcc2ae327944ee268590c0e266
Vasiliy <vasya2304@bk.ru>
2022-12-11 08:41:06 +0300

    чуть-чуть проапгрейдил форму бутстрапом

--- cities/forms.py
+++ cities/forms.py
@@ -9,7 +9,8 @@ class HtmlForm(forms.Form):
 
 
 class CityForm(forms.ModelForm):
-
+    name = forms.CharField(label='Город', widget=forms.TextInput(attrs={'class': 'form-control',
+                                                         'placeholder': 'Введите название города'}))
     class Meta():
         model = City
         fields = ('name', )
